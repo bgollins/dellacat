1f07cffbef4a9ad8839f82f370abda03b0c6aea7
Amelia Ralston-Okabayashi and Brittany Gollins <brittany@>
2018-09-05 19:42:12 -0700

    add blue border via click function to first murdering paragraph, remove border with Back button

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,13 @@ $(document).ready(function() {
       $("body").addClass("green-background");
       $("body").addClass("white-text");
   });
+
   $("button#back").click(function() {
     $("body").removeClass();
+    $("#border").removeClass();
+  });
+
+  $("#border").click(function() {
+    $("#border").addClass("border");
   });
 });
