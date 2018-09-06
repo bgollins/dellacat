84bef64b7da7e95f0c761e7dac8627f0b0b0fa9a
Amelia Ralston-Okabayashi and Brittany Gollins <brittany@>
2018-09-05 20:44:53 -0700

    add button for dog, cat, and draw conversation using prepend

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  $("#fade").fadeIn("slow", function() {
+
+  });
+
   $("button#green").click(function() {
       $("body").addClass("green-background");
       $("body").addClass("white-text");
@@ -12,4 +16,11 @@ $(document).ready(function() {
   $("#border").click(function() {
     $("#border").addClass("border");
   });
+
+  $("button#meow").click(function() {
+    $("ul#cat").prepend("<li>Meow!</li>");
+    
+  });
+
+
 });
